9785bf387005e2ddbb7dc6409330ec29814ccf65
Jovani Cadorna Warguez <jovaniwarguez21@gmail.com>
2017-10-04 21:43:48 +0800

    Update Assets Lib

--- lib/assets.php
+++ lib/assets.php
@@ -1,6 +1,9 @@
 <?php
 function assets() {
 
+  // Scripts
+  wp_enqueue_script( 'react-redux-js', get_stylesheet_directory_uri() . '/dist/build/bundle.js', array(), null, true);
+
 }
 add_action( 'wp_enqueue_scripts', 'assets' );
 ?>
