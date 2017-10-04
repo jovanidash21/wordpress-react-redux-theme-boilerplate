fd89af56b68f80b1689cbf2bf06e756ec8131c3f
Jovani Cadorna Warguez <jovaniwarguez21@gmail.com>
2017-10-04 21:35:10 +0800

    Add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var path = require('path');
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: path.join(__dirname, '/dist/build'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader!sass-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
