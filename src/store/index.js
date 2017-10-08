3c5c268d78a40b6f6636be38470ac917c50a61dd
Jovani Cadorna Warguez <jovaniwarguez21@gmail.com>
2017-10-08 21:53:37 +0800

    Add Redux Persist Config

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ import thunk from 'redux-thunk';
 import promiseMiddleware from 'redux-promise-middleware';
 import { routerMiddleware } from 'react-router-redux';
 import { createLogger } from 'redux-logger';
+import { persistStore, autoRehydrate } from 'redux-persist';
 import history from '../history';
 import reducers from '../reducers';
 
@@ -15,7 +16,10 @@ const store = createStore(
     promiseMiddleware(),
     reactRouterMiddleware,
     createLogger()
-  )
+  ),
+  autoRehydrate()
 );
+
+persistStore(store);
 
 export default store;
