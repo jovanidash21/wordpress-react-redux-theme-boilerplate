7d1bdb9862f3b9ef57d12497f2d0569943061e9d
Jovani Cadorna Warguez <jovaniwarguez21@gmail.com>
2017-10-14 22:54:37 +0800

    Add Browser Out-Of-Date Message

--- index.php
+++ index.php
@@ -4,6 +4,12 @@
     <?php wp_head(); ?>
   </head>
   <body>
+    <!--[if lt IE 9]>
+      <div class="alert alert-warning">
+        <?php echo 'Your browser is <strong>OUT-OF-DATE</strong>. Please <a href="http://browsehappy.com/">upgrade your browser</a> to view this website correctly.'; ?>
+      </div>
+    <![endif]-->
+
     <div id="root"></div>
     <?php wp_footer(); ?>
   </body>
