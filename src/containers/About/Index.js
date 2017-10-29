b1cbfc2eb4873c0dbfa2dcbd37573741312b68dc
Jovani Cadorna Warguez <jovaniwarguez21@gmail.com>
2017-10-29 22:42:45 +0800

    Update Redux Containers

--- src/containers/About/Index.js
+++ src/containers/About/Index.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Jumbotron } from 'reactstrap';
+import ReactHtmlParser, { 
+  processNodes, 
+  convertNodeToElement, 
+  htmlparser2 
+} from 'react-html-parser';
 import { fetchPage } from '../../actions/page';
 import Head from '../../components/Head';
 
@@ -28,6 +33,8 @@ class About extends Component {
                     <h2 className="display-3">
                       {data.title.rendered}
                     </h2>
+                    <hr className="my-2" />
+                    {ReactHtmlParser(data.content.rendered)}
                   </div>
                 )
                 : ''
