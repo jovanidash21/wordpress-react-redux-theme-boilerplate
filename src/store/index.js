9b0f746274966ab9315dd1312d1cc52777ce0aa4
Jovani Cadorna Warguez <jovaniwarguez21@gmail.com>
2017-10-29 20:01:34 +0800

    Update Redux Store

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,6 @@ import thunk from 'redux-thunk';
 import promiseMiddleware from 'redux-promise-middleware';
 import { routerMiddleware } from 'react-router-redux';
 import { createLogger } from 'redux-logger';
-import { persistStore, autoRehydrate } from 'redux-persist';
 import history from '../history';
 import reducers from '../reducers';
 
@@ -18,10 +17,7 @@ const store = createStore(
     }),
     reactRouterMiddleware,
     createLogger()
-  ),
-  autoRehydrate()
+  )
 );
-
-persistStore(store);
 
 export default store;
