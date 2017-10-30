c1e5276dd9e4c2911c225ea2b39fc9d7dbe3852d
Jovani Cadorna Warguez <jovaniwarguez21@gmail.com>
2017-10-30 23:26:39 +0800

    Add Work And Blog Single Container

---
+++ src/containers/Blog/Single.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Jumbotron } from 'reactstrap';
+import ReactHtmlParser from 'react-html-parser';
+import { fetchPost } from '../../actions/post';
+import Head from '../../components/Head';
+
+class BlogSingle extends Component {
+  constructor(props) {
+    super(props);
+  }
+  componentWillMount() {
+    this.props.dispatch(fetchPost('blog', this.props.match.params.slug));
+  }
+  render() {
+    const { post } = this.props;
+    const title = `${WP_REACT_REDUX.siteName} | Blog`;
+
+    return (
+      <div>
+        <Head title={title} />
+        <div className="container">
+          <Jumbotron>
+            {
+              post.data.length
+                ?
+                post.data.map((data, i) =>
+                  <div key={i}>
+                    <h3>Blog</h3>
+                    <hr className="my-2" />
+                    <h2 className="display-3">
+                      {data.title.rendered}
+                    </h2>
+                    <hr className="my-2" />
+                    {ReactHtmlParser(data.content.rendered)}
+                  </div>
+                )
+                : ''
+            }
+          </Jumbotron>
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {  
+  return {
+    post: state.post
+  }
+}
+
+export default connect(
+  mapStateToProps
+)(BlogSingle);
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,9 @@ import { Switch } from 'react-router';
 import Layout from '../containers/Layout';
 import Home from '../containers/Home';
 import Work from '../containers/Work';
+import WorkSingle from '../containers/Work/Single';
 import Blog from '../containers/Blog';
+import BlogSingle from '../containers/Blog/Single';
 import About from '../containers/About';
 import Contact from '../containers/Contact';
 
@@ -12,7 +14,9 @@ const routes = (
     <Switch>
       <Layout exact path="/" component={Home} />
       <Layout exact path="/work" component={Work} />
+      <Layout exact path="/work/:slug" component={WorkSingle} />
       <Layout exact path="/blog" component={Blog} />
+      <Layout exact path="/blog/:slug" component={BlogSingle} />
       <Layout exact path="/about" component={About} />
       <Layout exact path="/contact" component={Contact} />
     </Switch>
